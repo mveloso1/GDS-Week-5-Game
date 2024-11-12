00e4099d9acd95c0f9abb0150b64a1b02f8816a9
WolfiePie <jasmalls@email.neit.edu>
2024-11-12 13:42:39 -0500

    update

---
+++ frame.js
@@ -0,0 +1,11 @@
+const spider = document.getElementById('spider');
+const boss = document.getElementById('boss');
+const enemies = document.getElementById('enemies');
+const score = document.getElementById('score');
+
+function jump() {
+    spider.classList('jump-animation');
+}
+function run() {
+    spider.classList('run.animation');
+}
