e0ba4e2b62d33997b51c0db7351d81cab8536eaf
WolfiePie <jasmalls@email.neit.edu>
2024-11-12 13:50:08 -0500

    Add files via upload

--- frame.js
+++ frame.js
@@ -5,7 +5,16 @@ const score = document.getElementById('score');
 
 function jump() {
     spider.classList('jump-animation');
+    setTimeout(() => {
+        spider.classList.remove('jump-animation');
+    }, 500);
 }
 function run() {
     spider.classList('run.animation');
 }
+
+document.addEventListener('keypress', () => {
+    if(!spider.classList.contains('jump-animation')) {
+jump();
+    }
+})
